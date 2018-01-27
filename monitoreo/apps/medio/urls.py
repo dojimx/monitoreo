dcd04aeb13cba4a210756d3c9ab30df482fbde82
asmodeo95 <35041616+asmodeo95@users.noreply.github.com>
2018-01-27 11:52:08 -0700

    cambi

--- monitoreo/apps/medio/urls.py
+++ monitoreo/apps/medio/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path,include
-from apps.medio.views import index,listar,editar,eliminar
+from apps.medio.views import index,editar,eliminar,MedioList
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
    path('', index),
-   path('listar/', listar),
+   path('listar/', MedioList.as_view(), name='medio_listar'),
    path('editar/', editar),
    path('eliminar/', eliminar)
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
