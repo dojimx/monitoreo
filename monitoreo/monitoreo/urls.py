30503e5fde7726b9f946c93aee50f897c030927a
asmodeo95 <35041616+asmodeo95@users.noreply.github.com>
2018-01-29 10:53:50 -0700

    usuarios

--- monitoreo/monitoreo/urls.py
+++ monitoreo/monitoreo/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('medio/', include('apps.medio.urls')),
+    path('usuario/', include('apps.usuario.urls'))
 ]
